3014f62b0cb6310dad848d5cff811a16ee0a5807
MacBook Pro <macbookpro@MacBooks-MBP.Dlink>
2024-02-23 01:21:54 +0600

    create a || task added from || using react || hook | form

---
+++ src/components/tasks/AddedTaskModal.jsx
@@ -0,0 +1,72 @@
+import { useForm } from "react-hook-form";
+
+const AddedTaskModal = () => {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => console.log(data);
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-2 mt-3">
+        <label htmlFor="title">Title</label>
+        <input
+          className="w-full rounded-md"
+          id="title"
+          defaultValue="test"
+          {...register("title")}
+        />
+        <label htmlFor="description">Description</label>
+        <textarea
+          className="w-full rounded-md"
+          name=""
+          id="description"
+          cols="2"
+          rows="2"
+          {...register("description", { required: true })}
+        ></textarea>
+        <div>
+          <label htmlFor="deadline">Deadline</label>
+          <input
+            className="w-full rounded-md"
+            id="deadline"
+            type="date"
+            {...register("deadline", { required: true })}
+          />
+        </div>
+        <div>
+          <label htmlFor="priority">Priority</label>
+          <input
+            className="w-full rounded-md"
+            name="priority"
+            id="priority"
+            {...register("priority", { required: true })}
+          />
+        </div>
+        <div>
+          <label htmlFor="assignTo">Assign To</label>
+
+          <select
+            className="w-full rounded-md"
+            id="assignTo"
+            {...register("assignTo", { required: true })}
+          >
+            <option value="eren">Eren</option>
+            <option value="mikasa">Mikasa</option>
+            <option value="armin">Armin</option>
+            <option value="historia">Historia</option>
+            <option value="jean">Jean</option>
+            <option value="levi">Levi</option>
+            <option value="erwin">Erwin</option>
+            <option value="annie">Annie</option>
+          </select>
+        </div>
+        <div className="text-white flex items-center gap-5">
+          <button className="btn bg-primary">Submit</button>
+          <button className="btn bg-red-600">Cancel</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default AddedTaskModal;
--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -3,6 +3,7 @@ import MyTasks from "../components/tasks/MyTasks";
 import TaskCard from "../components/tasks/TaskCard";
 import Modal from "../components/ui/Modal";
 import { useState } from "react";
+import AddedTaskModal from "../components/tasks/AddedTaskModal";
 
 const Tasks = () => {
   let [isOpen, setIsOpen] = useState(false);
@@ -27,7 +28,9 @@ const Tasks = () => {
             >
               Add Task
             </button>
-            <Modal isOpen={isOpen} setIsOpen={setIsOpen} />
+            <Modal isOpen={isOpen} setIsOpen={setIsOpen} title="Added a Task!">
+              <AddedTaskModal />
+            </Modal>
             <div className="h-10 w-10 rounded-xl overflow-hidden">
               <img
                 src="https://images.unsplash.com/photo-1528892952291-009c663ce843?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=644&q=80"
